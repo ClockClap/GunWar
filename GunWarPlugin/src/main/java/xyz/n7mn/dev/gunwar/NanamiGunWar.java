e5d7c4990c296dcf7262a5697fb93b2ed739a51e
ClockClap <i.am.clockclap@gmail.com>
2021-06-13 02:18:03 +0900

    bugfix

--- GunWarPlugin/src/main/java/xyz/n7mn/dev/gunwar/NanamiGunWar.java
+++ GunWarPlugin/src/main/java/xyz/n7mn/dev/gunwar/NanamiGunWar.java
@@ -3,12 +3,15 @@ package xyz.n7mn.dev.gunwar;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.plugin.SimplePluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import xyz.n7mn.dev.api.Role;
 import xyz.n7mn.dev.gunwar.commands.AboutGunWarCommand;
 import xyz.n7mn.dev.gunwar.commands.GunWarReloadCommand;
 import xyz.n7mn.dev.gunwar.game.GunWarGame;
+import xyz.n7mn.dev.gunwar.game.data.GunWarPermanentlyPlayerData;
+import xyz.n7mn.dev.gunwar.game.data.GunWarPlayerData;
 import xyz.n7mn.dev.gunwar.game.data.PermanentlyPlayerData;
 import xyz.n7mn.dev.gunwar.game.data.PlayerData;
 import xyz.n7mn.dev.gunwar.game.gamemode.GwGameModes;
@@ -22,6 +25,7 @@ import xyz.n7mn.dev.gunwar.util.GwUtilities;
 import xyz.n7mn.dev.gunwar.util.NanamiGunWarConfiguration;
 import xyz.n7mn.dev.gunwar.util.PlayerWatcher;
 
+import java.io.File;
 import java.io.IOException;
 
 public final class NanamiGunWar extends JavaPlugin {
@@ -63,6 +67,35 @@ public final class NanamiGunWar extends JavaPlugin {
         pluginManager = (SimplePluginManager) Bukkit.getPluginManager();
         registerListeners();
         registerCommands();
+        for(Player p : Bukkit.getOnlinePlayers()) {
+            try {
+                GunWarPermanentlyPlayerData permanentlyPlayerData = new GunWarPermanentlyPlayerData(p.getUniqueId());
+                File f = permanentlyPlayerData.getDefaultDataFile();
+                if (!f.exists()) {
+                    f.createNewFile();
+                    permanentlyPlayerData.save(f);
+                } else {
+                    permanentlyPlayerData.load(f);
+                }
+                ((GunWarGame) GunWar.getGame()).addPermanentlyPlayerData(permanentlyPlayerData);
+            } catch(Throwable ex) {
+                p.kickPlayer("" +
+                        ChatColor.DARK_GREEN + "=== ななみ銃撃戦 ===\n" +
+                        ChatColor.RED + "接続が切れました。\n" +
+                        ChatColor.GRAY + "原因: " + ChatColor.WHITE + "プレイヤーデータ読み込み時のエラー発生\n" +
+                        ChatColor.GRAY + "解決策: " + ChatColor.WHITE + "Discordの" + ChatColor.BLUE + "#銃撃戦-バグ報告" + ChatColor.WHITE + "にて報告してください。\n" +
+                        "\n" +
+                        ChatColor.WHITE + "詳細はななみ鯖公式Discordをご確認ください。\n" +
+                        ChatColor.GOLD + "" + ChatColor.UNDERLINE + GunWar.getConfig().getConfig().getString("discord"));
+                ex.printStackTrace();
+            }
+            GunWarPlayerData data = new GunWarPlayerData(p);
+            PlayerWatcher watcher = new PlayerWatcher(GunWar.getPlugin(), data);
+            watcher.startWatch();
+            watcher.startWatch10Ticks();
+            data.setWatcher(watcher);
+            ((GunWarGame) GunWar.getGame()).addPlayerData(data.getUniqueId(), data);
+        }
     }
 
     private void registerListeners() {
